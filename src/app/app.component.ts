8ba7eabd9be0b806ff1a1a370dda1c9f786d6fe4
jiangyaqi2 <525168624@qq.com>
2017-04-23 22:51:34 +0800

    xx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component ,OnInit} from '@angular/core';
-import {Router} from '@angular/router'
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+// import {Router} from '@angular/router'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
